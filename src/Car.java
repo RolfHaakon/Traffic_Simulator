96c3683c8cf44575a08508628d439f2f64f4ca71
Rolf <rolfhaakon.rokseth@my.jcu.edu.au>
2019-08-22 15:32:24 +1000

    Added methods to car.java Added .png of UML

---
+++ src/Car.java
@@ -0,0 +1,31 @@
+public class Car {
+    int length = 6;
+    int width = 3;
+    int speed;
+    String color = "red";
+
+
+    /** Method for creating car */
+    public void buildCar(){
+        //spawn car
+
+
+    }
+
+    /** Method that initializes car movement */
+    public void start(){
+        speed = 1;
+    }
+
+    /** Method for stopping car */
+    public void stop(){
+        speed = 0;
+    }
+
+    /** Method for removing car */
+    public void removeCar(){
+        //remove car
+    }
+
+
+}
